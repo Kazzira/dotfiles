0a2b76456411324ea37f275077c427ee58f70c44
Zachary Dakota Meyer <dev.zdmeyer@gmail.com>
2024-10-26 08:03:02 -0500

    [doom]: Update config with another theme

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -63,7 +63,7 @@ If FILEXT is provided, return files with extension FILEXT instead."
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'deeper-blue)
 
 ;; Set the font sizes for the org levels.
 (custom-set-faces
